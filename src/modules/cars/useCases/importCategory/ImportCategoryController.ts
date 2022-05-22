d0f2a2c61c2302daf4ee9cd6a4a9a3d711bc3d32
Fabiano Brancher <fabiano.brancher@hotmail.com>
2022-05-22 00:16:52 -0300

    ImportCategoryController swagger

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -8,10 +8,10 @@ class ImportCategoryController {
     async handle(request: Request, response: Response): Promise<Response> {
         const importCategoryUseCase = container.resolve(ImportCategoryuseCase);
         const { file } = request;
-        
+
         await importCategoryUseCase.execute(file);
-        
-        return response.send();
+
+        return response.status(201).send();
     }
 }
 
